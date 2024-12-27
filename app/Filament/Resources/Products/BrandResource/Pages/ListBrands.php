6dea5303fa87bdaaae89e8702d24dd6169f2357f
maoloape <151963939+maoloape@users.noreply.github.com>
2024-12-27 09:49:35 +0700

    Utensil-Indo

---
+++ app/Filament/Resources/Products/BrandResource/Pages/ListBrands.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Products\BrandResource\Pages;
+
+use App\Filament\Resources\Products\BrandResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListBrands extends ListRecords
+{
+    protected static string $resource = BrandResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
